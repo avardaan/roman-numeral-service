ca44ab799b812db44298357b48715fd659a955d5
avardaan <oyevardaan@gmail.com>
2023-08-02 22:33:59 -0700

    complete working mvp

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,31 @@
 
 import express, { type Express, Request, Response } from 'express';
 import { RomanNumeralRequestQueryParams, RomanNumeralResponseBody } from './types';
+import { intToRoman } from './lib';
 
-// express app
+// define port for server to bind to
+const PORT: number = parseInt(process.env.PORT) || 8080;
+
+// initialize express app
 const app: Express = express();
 
 app.get(
 	'/romannumeral',
 	(req: Request<{}, {}, {}, RomanNumeralRequestQueryParams>, res: Response) => {
 		// get query param as string
-		const arabicNumberAsString: string = req.query.query;
+		const inputIntAsString: string = req.query.query;
 		// convert query param to number
-		// const arabicNumber: number = parseInt(arabicNumberAsString, 10);
+		const inputInt: number = parseInt(inputIntAsString, 10);
 
 		const response: RomanNumeralResponseBody = {
-			input: arabicNumberAsString,
-			output: 'I',
+			input: inputIntAsString,
+			output: intToRoman(inputInt),
 		};
-
 		res.json(response);
 	}
 );
 
 // listen for requests
-app.listen(3000, () => {
-	console.log('Server started on port 3000');
+app.listen(PORT, () => {
+	console.log(`server started on port ${PORT}`);
 });
